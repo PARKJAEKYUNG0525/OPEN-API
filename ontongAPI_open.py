dbb6ef1c863bcd207c8128b598f7bdf4048602a5
PARKJAEKYUNG <pjg0525@naver.com>
2026-07-03 12:59:22 +0900

    api 키수정

--- ontongAPI_open.py
+++ ontongAPI_open.py
@@ -1,24 +1,28 @@
-# 공공 - 행안부 - 목록
 import requests
 import json
+import os
+from dotenv import load_dotenv
 
+# .env 파일 로드
+load_dotenv()
+
+# 환경변수 가져오기
+ontong_api_key = os.getenv("ONTONG_API_KEY")
 
 url = "https://www.youthcenter.go.kr/go/ythip/getPlcy"
 
 params = {
-    "apiKeyNm": apiKeyNm,
+    "apiKeyNm": ontong_api_key,
     "pageNum": 1,
-    "pageSize": 2600,
+    "pageSize": 2632,
     "pageType": "2",
     "rtnType": "json"
 }
 
 response = requests.get(url, params=params)
-
 data = response.json()
 
-# 보기 좋게 저장
-with open("support_data_2600.json", "w", encoding="utf-8") as f:
+with open("support_data_2632.json", "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
 print("저장 완료")
